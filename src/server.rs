c3c387e6183c11b141a9eabd9015987bb10b1bf4
Aidan Shilling <aidanshilling91@gmail.com>
2022-08-22 20:16:35 -0400

    Add query string parsing

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,5 @@
 use crate::http::Request;
 use std::convert::TryFrom;
-use std::convert::TryInto;
 use std::io::Read;
 use std::net::TcpListener;
 
@@ -27,7 +26,9 @@ impl Server {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
 
                             match Request::try_from(&buffer[..]) {
-                                Ok(request) => {}
+                                Ok(request) => {
+                                    dbg!(request);
+                                }
                                 Err(e) => println!("Failed to parse a request: {}", e),
                             }
                         }
